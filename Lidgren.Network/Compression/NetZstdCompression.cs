b659afcf4cc47644b81dbc47147f2b2ad5625d9b
Vera Aguilera Puerto <zddm@outlook.es>
2020-12-25 15:12:33 +0100

    Disposable compression

--- Lidgren.Network/Compression/NetZstdCompression.cs
+++ Lidgren.Network/Compression/NetZstdCompression.cs
@@ -1,19 +1,27 @@
+#nullable enable
 using System;
 using ZstdNet;
 
 namespace Lidgren.Network
 {
-    public class NetZstdCompression : NetCompression
+    public class NetZstdCompression : NetCompression, IDisposable
     {
-        private readonly Compressor _compressor = new Compressor(new CompressionOptions(CompressionOptions.MaxCompressionLevel));
-        private readonly Decompressor _decompressor = new Decompressor();
+	    private bool _disposed = false;
+        private readonly Compressor _compressor;
+        private readonly Decompressor _decompressor;
+
+        public NetZstdCompression(byte[]? dictionary = null, int compressionLevel = CompressionOptions.DefaultCompressionLevel)
+        {
+	        _compressor = new Compressor(new CompressionOptions(dictionary));
+	        _decompressor = new Decompressor();
+        }
 
         public override bool Compress(NetOutgoingMessage msg)
         {
+	        if (_disposed) return false;
+	        
             var span = msg.Data.AsSpan().Slice(msg.PositionInBytes, msg.LengthBytes);
-
             var data = _compressor.Wrap(span);
-            
             msg.Data = data;
             msg.m_bitLength = data.Length * 8;
             return true;
@@ -21,12 +29,21 @@ namespace Lidgren.Network
 
         public override bool Decompress(NetIncomingMessage msg)
         {
+	        if (_disposed) return false;
+	        
             var span = msg.Data.AsSpan().Slice(msg.PositionInBytes, msg.LengthBytes);
-
             var data = _decompressor.Unwrap(span);
             msg.Data = data;
             msg.m_bitLength = data.Length * 8;
             return true;
+        }
+
+        public void Dispose()
+        {
+	        if (_disposed) return;
+	        _disposed = true;
+	        _compressor.Dispose();
+	        _decompressor.Dispose();
         }
     }
 }
