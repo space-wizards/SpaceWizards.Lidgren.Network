620bd7a960a05381124200af7b997affaf819c29
Pieter-Jan Briers <pieterjan.briers@gmail.com>
2021-10-14 18:31:37 +0200

    Attempt to make macOS fast sockets working.
    Can't get this to work, committing this so I don't lose it and giving up.

--- Lidgren.Network/Sockets/NetFastSocket.cs
+++ Lidgren.Network/Sockets/NetFastSocket.cs
@@ -11,14 +11,14 @@ namespace Lidgren.Network
 	{
 		// .NET's built-in socket datagram APIs allocate constantly due to poor design.
 		// These don't thanks to usage of a custom NetSocketAddress.
-		
+
 		public static int SendTo(Socket socket, ReadOnlySpan<byte> buffer, SocketFlags socketFlags, IPEndPoint endPoint)
 		{
-			var socketAddress = (NetSocketAddress)endPoint;
+			var socketAddress = (NetSocketAddress) endPoint;
 
 			return SendTo(socket, buffer, socketFlags, socketAddress);
 		}
-		
+
 		public static int SendTo(
 			Socket socket,
 			ReadOnlySpan<byte> buffer,
@@ -28,62 +28,114 @@ namespace Lidgren.Network
 			if (socketFlags != SocketFlags.None)
 				throw new ArgumentException("Non-none socket flags not currently supported.");
 
-			sockaddr* address;
-			int addressLen;
-
-			if (socketAddress.Family == NetIpAddressFamily.V6)
-			{
-				sockaddr_in6 address6 = default;
-
-				ref var refAddress6 = ref Unsafe.AsRef(socketAddress.V6);
-
-				address6.sin6_port = htons(refAddress6.Port);
-				address6.sin6_family = AF_INET6;
-				address6.sin6_scope_id = refAddress6.ScopeId;
-				address6.sin6_addr = Unsafe.As<NetIpv6Address, in6_addr>(ref refAddress6.Address);
-
-				address = (sockaddr*)(&address6);
-				addressLen = sizeof(sockaddr_in6);
-			}
-			else
-			{
-				Debug.Assert(socketAddress.Family == NetIpAddressFamily.V4);
-
-				ref var refAddress4 = ref Unsafe.AsRef(socketAddress.V4);
-
-				sockaddr_in address4 = default;
-				address4.sin_port = htons(refAddress4.Port);
-				address4.sin_family = AF_INET;
-				address4.sin_addr = Unsafe.As<NetIpv4Address, in_addr>(ref refAddress4.Address);
-
-				address = (sockaddr*)(&address4);
-				addressLen = sizeof(sockaddr_in);
-			}
-
 			int ret;
-			fixed (byte* bufPtr = buffer)
+
+			if (IsFreeBsd || IsMacOS)
 			{
-				if (IsWindows)
+				sockaddr_bsd* address;
+				int addressLen;
+
+				if (socketAddress.Family == NetIpAddressFamily.V6)
 				{
-					ret = sendto_win32(
-						socket.Handle,
-						bufPtr,
-						buffer.Length,
-						(int)socketFlags,
-						address,
-						addressLen);
+					sockaddr_in6_bsd address6 = default;
+
+					ref var refAddress6 = ref Unsafe.AsRef(socketAddress.V6);
+
+					address6.sin6_len = (byte) sizeof(sockaddr_in6_bsd);
+					address6.sin6_port = htons(refAddress6.Port);
+					address6.sin6_family = AF_INET6;
+					address6.sin6_scope_id = refAddress6.ScopeId;
+					address6.sin6_addr = Unsafe.As<NetIpv6Address, in6_addr>(ref refAddress6.Address);
+
+					address = (sockaddr_bsd*) (&address6);
+					addressLen = sizeof(sockaddr_in6_bsd);
 				}
 				else
 				{
-					ret = (int)sendto_posix(
-						(int)socket.Handle,
+					Debug.Assert(socketAddress.Family == NetIpAddressFamily.V4);
+
+					ref var refAddress4 = ref Unsafe.AsRef(socketAddress.V4);
+
+					sockaddr_in_bsd address4 = default;
+					address4.sin_len = (byte) sizeof(sockaddr_in_bsd);
+					address4.sin_port = htons(refAddress4.Port);
+					address4.sin_family = AF_INET;
+					address4.sin_addr = Unsafe.As<NetIpv4Address, in_addr>(ref refAddress4.Address);
+
+					address = (sockaddr_bsd*) (&address4);
+					addressLen = sizeof(sockaddr_in_bsd);
+				}
+
+				fixed (byte* bufPtr = buffer)
+				{
+					ret = (int) sendto_bsd(
+						(int) socket.Handle,
 						bufPtr,
-						(IntPtr)buffer.Length,
-						(int)socketFlags,
+						(IntPtr) buffer.Length,
+						(int) socketFlags,
 						address,
-						(uint)addressLen);
+						(uint) addressLen);
 				}
 			}
+			else
+			{
+				sockaddr* address;
+				int addressLen;
+
+				if (socketAddress.Family == NetIpAddressFamily.V6)
+				{
+					sockaddr_in6 address6 = default;
+
+					ref var refAddress6 = ref Unsafe.AsRef(socketAddress.V6);
+
+					address6.sin6_port = htons(refAddress6.Port);
+					address6.sin6_family = AF_INET6;
+					address6.sin6_scope_id = refAddress6.ScopeId;
+					address6.sin6_addr = Unsafe.As<NetIpv6Address, in6_addr>(ref refAddress6.Address);
+
+					address = (sockaddr*) (&address6);
+					addressLen = sizeof(sockaddr_in6);
+				}
+				else
+				{
+					Debug.Assert(socketAddress.Family == NetIpAddressFamily.V4);
+
+					ref var refAddress4 = ref Unsafe.AsRef(socketAddress.V4);
+
+					sockaddr_in address4 = default;
+					address4.sin_port = htons(refAddress4.Port);
+					address4.sin_family = AF_INET;
+					address4.sin_addr = Unsafe.As<NetIpv4Address, in_addr>(ref refAddress4.Address);
+
+					address = (sockaddr*) (&address4);
+					addressLen = sizeof(sockaddr_in);
+				}
+
+				fixed (byte* bufPtr = buffer)
+				{
+					if (IsWindows)
+					{
+						ret = sendto_win32(
+							socket.Handle,
+							bufPtr,
+							buffer.Length,
+							(int) socketFlags,
+							address,
+							addressLen);
+					}
+					else
+					{
+						ret = (int) sendto_linux(
+							(int) socket.Handle,
+							bufPtr,
+							(IntPtr) buffer.Length,
+							(int) socketFlags,
+							address,
+							(uint) addressLen);
+					}
+				}
+			}
+
 
 			if (ret != -1)
 				return ret;
@@ -104,71 +156,122 @@ namespace Lidgren.Network
 		public static int ReceiveFrom(
 			Socket socket,
 			Span<byte> buffer,
-			SocketFlags socketFlags, 
+			SocketFlags socketFlags,
 			out NetSocketAddress socketAddress)
 		{
 			if (socketFlags != SocketFlags.None)
 				throw new ArgumentException("Non-none socket flags not currently supported.");
 
-			sockaddr_in6 address6;
-			
 			int ret;
-			fixed (byte* bufPtr = buffer)
+
+			if (IsFreeBsd || IsMacOS)
 			{
-				if (IsWindows)
+				sockaddr_in6_bsd address6;
+				address6.sin6_len = (byte) sizeof(sockaddr_in6_bsd);
+
+				fixed (byte* bufPtr = buffer)
 				{
-					var len = sizeof(sockaddr_in6);
-					ret = recvfrom_win32(
-						socket.Handle,
+					var len = (uint) sizeof(sockaddr_in6_bsd);
+					ret = (int) recvfrom_bsd(
+						(int) socket.Handle,
 						bufPtr,
-						buffer.Length,
-						(int)socketFlags,
-						(sockaddr*) (&address6),
+						(IntPtr) buffer.Length,
+						(int) socketFlags,
+						(sockaddr_bsd*) (&address6),
 						&len);
 				}
-				else
+
+				if (ret != -1)
 				{
-					var len = (uint)sizeof(sockaddr_in6);
-					ret = (int)recvfrom_posix(
-						(int)socket.Handle,
-						bufPtr,
-						(IntPtr)buffer.Length,
-						(int)socketFlags,
-						(sockaddr*) (&address6),
-						&len);
+					if (address6.sin6_family == AF_INET6)
+					{
+						// IPv6
+						socketAddress = new NetSocketAddressV6
+						{
+							Address = Unsafe.As<in6_addr, NetIpv6Address>(ref address6.sin6_addr),
+							Port = ntohs(address6.sin6_port),
+							ScopeId = address6.sin6_scope_id
+						};
+					}
+					else if (address6.sin6_family == AF_INET)
+					{
+						// IPv4
+						ref var address4 = ref Unsafe.As<sockaddr_in6_bsd, sockaddr_in_bsd>(ref address6);
+						socketAddress = new NetSocketAddressV4
+						{
+							Address = Unsafe.As<in_addr, NetIpv4Address>(ref address4.sin_addr),
+							Port = ntohs(address4.sin_port),
+						};
+					}
+					else
+					{
+						throw new SocketException((int) SocketError.ProtocolFamilyNotSupported);
+					}
+
+					return ret;
+				}
+			}
+			else
+			{
+				sockaddr_in6 address6;
+
+				fixed (byte* bufPtr = buffer)
+				{
+					if (IsWindows)
+					{
+						var len = sizeof(sockaddr_in6);
+						ret = recvfrom_win32(
+							socket.Handle,
+							bufPtr,
+							buffer.Length,
+							(int) socketFlags,
+							(sockaddr*) (&address6),
+							&len);
+					}
+					else
+					{
+						var len = (uint) sizeof(sockaddr_in6);
+						ret = (int) recvfrom_linux(
+							(int) socket.Handle,
+							bufPtr,
+							(IntPtr) buffer.Length,
+							(int) socketFlags,
+							(sockaddr*) (&address6),
+							&len);
+					}
+				}
+
+				if (ret != -1)
+				{
+					if (address6.sin6_family == AF_INET6)
+					{
+						// IPv6
+						socketAddress = new NetSocketAddressV6
+						{
+							Address = Unsafe.As<in6_addr, NetIpv6Address>(ref address6.sin6_addr),
+							Port = ntohs(address6.sin6_port),
+							ScopeId = address6.sin6_scope_id
+						};
+					}
+					else if (address6.sin6_family == AF_INET)
+					{
+						// IPv4
+						ref var address4 = ref Unsafe.As<sockaddr_in6, sockaddr_in>(ref address6);
+						socketAddress = new NetSocketAddressV4
+						{
+							Address = Unsafe.As<in_addr, NetIpv4Address>(ref address4.sin_addr),
+							Port = ntohs(address4.sin_port),
+						};
+					}
+					else
+					{
+						throw new SocketException((int) SocketError.ProtocolFamilyNotSupported);
+					}
+
+					return ret;
 				}
 			}
 
-			if (ret != -1)
-			{
-				if (address6.sin6_family == AF_INET6)
-				{
-					// IPv6
-					socketAddress = new NetSocketAddressV6
-					{
-						Address = Unsafe.As<in6_addr, NetIpv6Address>(ref address6.sin6_addr),
-						Port = ntohs(address6.sin6_port),
-						ScopeId = address6.sin6_scope_id
-					};
-				}
-				else if (address6.sin6_family == AF_INET)
-				{
-					// IPv4
-					ref var address4 = ref Unsafe.As<sockaddr_in6, sockaddr_in>(ref address6);
-					socketAddress = new NetSocketAddressV4
-					{
-						Address = Unsafe.As<in_addr, NetIpv4Address>(ref address4.sin_addr),
-						Port = ntohs(address4.sin_port),
-					};
-				}
-				else
-				{
-					throw new SocketException((int)SocketError.ProtocolFamilyNotSupported);
-				}
-
-				return ret;
-			}
-			
 			// Errors occured
 			if (IsWindows)
 			{
--- Lidgren.Network/Sockets/NetNativeSocket.cs
+++ Lidgren.Network/Sockets/NetNativeSocket.cs
@@ -7,6 +7,8 @@ namespace Lidgren.Network
 	internal static unsafe class NetNativeSocket
 	{
 		internal static readonly bool IsWindows = RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+		internal static readonly bool IsMacOS = RuntimeInformation.IsOSPlatform(OSPlatform.OSX);
+		internal static readonly bool IsFreeBsd = RuntimeInformation.IsOSPlatform(OSPlatform.FreeBSD);
 
 #pragma warning disable 649
 		// ReSharper disable InconsistentNaming
@@ -14,7 +16,11 @@ namespace Lidgren.Network
 		// ReSharper disable StringLiteralTypo
 
 		internal static readonly ushort AF_INET = 2;
-		internal static readonly ushort AF_INET6 = (ushort) (IsWindows ? 23 : 10);
+		// Linux: 10
+		// Windows: 23
+		// FreeBSD: 28
+		// macOS: 30
+		internal static readonly ushort AF_INET6 = (ushort) (IsWindows ? 23 : IsMacOS ? 30 : IsFreeBsd ? 28 : 10);
 		
 		internal struct sockaddr
 		{
@@ -22,8 +28,24 @@ namespace Lidgren.Network
 			public fixed byte sa_data[14];
 		}
 
+		internal struct sockaddr_bsd
+		{
+			public byte sa_len;
+			public ushort sa_family;
+			public fixed byte sa_data[14];
+		}
+		
 		internal struct sockaddr_in
 		{
+			public ushort sin_family;
+			public ushort sin_port;
+			public in_addr sin_addr;
+			public fixed byte sin_zero[8];
+		}
+		
+		internal struct sockaddr_in_bsd
+		{
+			public ushort sin_len;
 			public ushort sin_family;
 			public ushort sin_port;
 			public in_addr sin_addr;
@@ -37,6 +59,16 @@ namespace Lidgren.Network
 
 		internal struct sockaddr_in6
 		{
+			public ushort sin6_family;
+			public ushort sin6_port;
+			public uint sin6_flowinfo;
+			public in6_addr sin6_addr;
+			public uint sin6_scope_id;
+		}
+
+		internal struct sockaddr_in6_bsd
+		{
+			public byte sin6_len;
 			public ushort sin6_family;
 			public ushort sin6_port;
 			public uint sin6_flowinfo;
@@ -70,12 +102,21 @@ namespace Lidgren.Network
 				: netlong;
 
 		[DllImport("libc", EntryPoint = "sendto")]
-		internal static extern IntPtr sendto_posix(
+		internal static extern IntPtr sendto_linux(
 			int sockfd,
 			void* buf,
 			IntPtr len,
 			int flags,
 			sockaddr* dest_addr,
+			uint addrlen);
+		
+		[DllImport("libc", EntryPoint = "sendto")]
+		internal static extern IntPtr sendto_bsd(
+			int sockfd,
+			void* buf,
+			IntPtr len,
+			int flags,
+			sockaddr_bsd* dest_addr,
 			uint addrlen);
 
 		[DllImport("Ws2_32.dll", EntryPoint = "sendto")]
@@ -88,7 +129,7 @@ namespace Lidgren.Network
 			int tolen);
 
 		[DllImport("libc", EntryPoint = "recvfrom")]
-		internal static extern IntPtr recvfrom_posix(
+		internal static extern IntPtr recvfrom_linux(
 			int sockfd,
 			void* buf,
 			IntPtr len,
@@ -96,6 +137,15 @@ namespace Lidgren.Network
 			sockaddr* src_addr,
 			uint* addrlen);
 
+		[DllImport("libc", EntryPoint = "recvfrom")]
+		internal static extern IntPtr recvfrom_bsd(
+			int sockfd,
+			void* buf,
+			IntPtr len,
+			int flags,
+			sockaddr_bsd* src_addr,
+			uint* addrlen);
+		
 		[DllImport("Ws2_32.dll", EntryPoint = "recvfrom")]
 		internal static extern int recvfrom_win32(
 			IntPtr s,
