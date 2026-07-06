3619afc23aba91d93d38ba0ff7d3d4d21afc97a7
metalgearsloth <31366439+metalgearsloth@users.noreply.github.com>
2026-07-06 23:48:32 +1000

    Validate expand MTU values (#38)

--- Lidgren.Network/NetPeerConfiguration.cs
+++ Lidgren.Network/NetPeerConfiguration.cs
@@ -581,7 +581,12 @@ namespace Lidgren.Network
 		public float ExpandMTUFrequency
 		{
 			get { return m_expandMTUFrequency; }
-			set { m_expandMTUFrequency = value; }
+			set
+			{
+				if (!float.IsFinite(value) || value <= 0)
+					throw new NetException("ExpandMTUFrequency must be greater than zero");
+				m_expandMTUFrequency = value;
+			}
 		}
 
 		/// <summary>
@@ -590,7 +595,12 @@ namespace Lidgren.Network
 		public int ExpandMTUFailAttempts
 		{
 			get { return m_expandMTUFailAttempts; }
-			set { m_expandMTUFailAttempts = value; }
+			set
+			{
+				if (value <= 0)
+					throw new NetException("ExpandMTUFailAttempts must be greater than zero");
+				m_expandMTUFailAttempts = value;
+			}
 		}
 
 		/// <summary>
